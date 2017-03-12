0056594b952464dc03971a7c4af394dc9d90d265
Richard Kämmerer <richard@richardkaemmerer.de>
2017-03-13 00:21:02 +0100

    dict_factory

--- database/setup.py
+++ database/setup.py
@@ -1,5 +1,13 @@
 import sqlite3
 
+#: from http://stackoverflow.com/questions/811548/sqlite-and-python-return-a-dictionary-using-fetchone
+def dict_factory(cursor, row):
+    d = {}
+    for idx,col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+
+    return d
+
 def setup_database(sqlite_file, dbversion):
 
     conn = sqlite3.connect(sqlite_file)
