0503803be3de9bf6daa575e045602364c5b75dcc
Richard Kämmerer <richard@richardkaemmerer.de>
2017-03-14 17:26:38 +0100

    Ausgangsrechnungen: bezahlt, gedruckt

--- database/rechnungen.py
+++ database/rechnungen.py
@@ -33,7 +33,7 @@ def save_rechnung_step1(sqlite_file, rechnung):
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
 
-    c.execute("INSERT INTO rechnungen (rechnungsnummer, kunden_id, rechnungsdatum, zahlungsart, zahlungsstatus) VALUES (?, ?, ?, ?, ?)", [ rechnung['rechnungsnummer'], rechnung['kunden_id'], rechnung['rechnungsdatum'], rechnung['zahlungsart'], rechnung['zahlungsstatus'] ] )
+    c.execute("INSERT INTO rechnungen (rechnungsnummer, kunden_id, rechnungsdatum, zahlungsart, zahlungsstatus, gedruckt) VALUES (?, ?, ?, ?, ?)", [ rechnung['rechnungsnummer'], rechnung['kunden_id'], rechnung['rechnungsdatum'], rechnung['zahlungsart'], rechnung['zahlungsstatus'], 0 ] )
 
     conn.commit()
     conn.close()
