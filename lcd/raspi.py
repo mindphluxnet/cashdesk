86a1d0c9d8e9e6c95423cd23a162de11dcceea84
Richard Kämmerer <richard@richardkaemmerer.de>
2017-03-15 17:08:08 +0100

    Kommentar zur Pi-Detection

--- lcd/raspi.py
+++ lcd/raspi.py
@@ -1,5 +1,8 @@
 def is_raspi():
-
+    #: the file /sys/firmware/devicetree/base/model only exists on a Raspberry Pi
+    #: and contains the exact model and revision
+    #: just to be sure, we'll not only check for existance, but also check if the file
+    #: actually contains the string "Raspberry Pi"
     try:
         f = open("/sys/firmware/devicetree/base/model")
         data = f.read()
