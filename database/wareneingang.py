d38d03291fbf581ec0f9a1f572b674e9884f2787
Richard Kämmerer <richard@richardkaemmerer.de>
2017-03-20 10:59:22 +0100

    Wareneingang

---
+++ database/wareneingang.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+import database.factory
+
+def load_wareneingang(sqlite_file, rechnung_id):
+
+    conn = sqlite3.connect(sqlite_file)
+    conn.row_factory = database.factory.dict_factory
+    c = conn.cursor()
+
+    c.execute("SELECT oid, * FROM wareneingang WHERE rechnung_id = ? ORDER BY oid DESC")
+    wareneingang = c.fetchall()
+
+    conn.close()
+
+    return wareneingang
